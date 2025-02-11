0d13f8a686b7050215cb658e07443ab9aac6fb90
Mohammed Feroz <mohammed.feroz@keka.com>
2025-02-11 14:42:01 +0530

    Added allure reports, role remove method, verify toaster message and few nore fixes

--- Keka-engage/config/constants.ts
+++ Keka-engage/config/constants.ts
@@ -1,6 +1,13 @@
 // src/config/config.ts
 export const BASE_URL = 'https://login.kekastage.com/';
-export const Anno_URL =  'https://aarthi.kekastage.com/#/engage/announcements/list'
+export const Dashboard_URL = 'https://aarthi.kekastage.com/#/home/dashboard';
+export const Anno_URL =  'https://aarthi.kekastage.com/#/engage/announcements/list';
+export const Published_toaster = 'Success!Announcement published successfully.';
+export const Deleted_toaster = 'Success!Announcement deleted successfully.';
+export const Updated_toaster = 'Success!Announcement updated successfully.';  
+export const Missing_title = 'Warning!!Title and Description are mandatory.';
+export const Draft_toaster = 'Success!!Announcement saved successfully.';
+export const Role_updated = 'Success!!Role updated successfully.';
 export const CREDENTIALS = {
   admin: { username: 'aarthi.m@keka.com', password: 'Aarthi16' },
   employee: { username: 'emp_user', password: 'emp_pass' },
